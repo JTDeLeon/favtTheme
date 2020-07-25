cf5cb892a15eb9de316dcf0ca8002846ac0aca0f
Jonathan <jondeleon856@gmail.com>
2020-07-25 19:07:22 -0400

    initial commit

---
+++ homepage.php
@@ -0,0 +1,17 @@
+<?php
+/** 
+ * Template Name: Custom Homepage
+ */
+
+get_header();
+?>
+
+	<div>
+		<h1>
+		Homepage
+		</h1>
+	</div>
+
+	
+<?php
+get_footer();
---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * jonathan_underscore Theme Customizer
+ *
+ * @package jonathan_underscore
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function jonathan_underscore_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'jonathan_underscore_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'jonathan_underscore_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'jonathan_underscore_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function jonathan_underscore_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function jonathan_underscore_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function jonathan_underscore_customize_preview_js() {
+	wp_enqueue_script( 'jonathan_underscore-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'jonathan_underscore_customize_preview_js' );
