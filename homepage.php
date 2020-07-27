939cbe165264155ad0ca182a3027b3c5ee1e4388
Jonathan <jondeleon856@gmail.com>
2020-07-27 12:51:21 -0400

    update homepage; git

--- homepage.php
+++ homepage.php
@@ -6,11 +6,17 @@
 get_header();
 ?>
 
-	<div>
-		<h1>
-		This is the Homepage
-		</h1>
-	</div>
+	<section class="homepage-select">
+		<div class="centered-area">
+			<h1>We Connect Teachers to Students Online!</h1>
+			<p>Join Now For Free!</p>
+			<div class="two-col">
+				<a class="join-btn" href="#">Students</a>
+				<a class="join-btn" href="/sign-up">Teachers</a>
+			</div>
+
+		</div>
+	</section>
 
 	
 <?php
