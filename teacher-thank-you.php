e4375ca48fcc152a6fd7154b659ab37b17558175
Jonathan <jondeleon856@gmail.com>
2020-07-27 17:39:03 -0400

    built tank you page

---
+++ teacher-thank-you.php
@@ -0,0 +1,23 @@
+<?php
+/** 
+ * Template Name: Teacher Thank You
+ */
+
+get_header();
+?>
+
+	<section class="homepage-select">
+		<div class="centered-area">
+			<h1>Thank you for signing up, someone will be in contact with you within the next 24 business hours.</h1>
+			<p></p>
+			<!-- <div class="two-col">
+				<a class="join-btn" href="#">Students</a>
+				<a class="join-btn" href="/teacher-sign-up">Teachers</a>
+			</div> -->
+
+		</div>
+	</section>
+
+	
+<?php
+get_footer();
