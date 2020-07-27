645911529a0a8f2b5c9151d75d66f0dd5d88ad67
Jonathan <jondeleon856@gmail.com>
2020-07-27 16:53:25 -0400

    update contact info

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			
+			<p>Have additional questions? Please email us at: swarttig@gmail.com</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
